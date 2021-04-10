845cef94cde6ba2ccd7cee22d3d56cc027c2aac2
ACwojdziDC <WDgithub1978*>
2021-04-10 15:34:39 +0200

    bank Manager

---
+++ Polymorphism/BankManager/Bank.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism.BankManager
+{
+    class Bank
+    {
+        List<Account> bank;
+
+        public Bank(List<Account> list)
+        {
+            bank = list;
+        }
+
+        public decimal Total()
+        {
+            decimal total = 0;
+            foreach(Account element in bank)
+            {
+
+                if (element is SavingAccount e1)
+                {
+                    //SavingAccount e1 = (SavingAccount)element;
+                    total = total + e1.balance + e1.interest;
+                }
+                else if (element is Account)
+                {
+                    total += element.balance;
+                }
+
+            }
+            return total;
+        }
+    }
+}
--- Polymorphism/Program.cs
+++ Polymorphism/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Polymorphism.BankManager;
+
+using System.Collections.Generic;
 
 namespace Polymorphism
 {
@@ -6,13 +9,26 @@ namespace Polymorphism
     {
         static void Main(string[] args)
         {
-            OrderItem apple = new OrderItem("apple", 2, (decimal)3.50);
+            Account a1 = new Account();
+            a1.balance = 20;
+            SavingAccount a2 = new SavingAccount();
+            a2.balance = 10;
+            a2.interest = 2;
+            List<Account> list = new List<Account> { a1, a2 };
+
+
+            Bank bank = new Bank(list);
+            Console.WriteLine(bank.Total());
+
+
+
+            /*OrderItem apple = new OrderItem("apple", 2, (decimal)3.50);
             OrderItemWithPackage eggs = new OrderItemWithPackage("eggs", 10, 6, "package", 2);
             Order order = new Order();
             
             order.AddItemToList(apple);
             order.AddItemToList(eggs);
-            order.PrintOrder();
+            order.PrintOrder();*/
         }
     }
 }
